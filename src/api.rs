097abbc9fcee9f914899171dd9376a13f1587ad6
Alexander Sisco <alexander@render.com>
2024-10-03 16:28:41 -0700

    update to actix.rs 4.9; updated todo example with sqlx

--- src/api.rs
+++ src/api.rs
@@ -1,172 +1,148 @@
 use actix_files::NamedFile;
 use actix_session::Session;
-use actix_web::middleware::errhandlers::ErrorHandlerResponse;
-use actix_web::{dev, error, http, web, Error, HttpResponse, Responder, Result};
-use futures::future::{err, Either, Future, IntoFuture};
-use tera::{escape_html, Context, Tera};
+use actix_web::{
+    dev, error, http::StatusCode, middleware::ErrorHandlerResponse, web, Error, HttpResponse,
+    Responder, Result,
+};
+use serde::Deserialize;
+use sqlx::PgPool;
+use tera::{Context, Tera};
 
-use crate::db;
-use crate::session::{self, FlashMessage};
+use crate::{
+    db,
+    session::{self, FlashMessage},
+};
 
-pub fn index(
-    pool: web::Data<db::PgPool>,
+pub async fn index(
+    pool: web::Data<PgPool>,
     tmpl: web::Data<Tera>,
     session: Session,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || db::get_all_tasks(&pool))
-        .from_err()
-        .then(move |res| match res {
-            Ok(tasks) => {
-                let mut context = Context::new();
-                let escaped_tasks = tasks
-                    .iter()
-                    .map(|task| task.html_escaped())
-                    .collect::<Vec<_>>();
-                context.insert("tasks", &escaped_tasks);
+) -> Result<HttpResponse, Error> {
+    let tasks = db::get_all_tasks(&pool)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
 
-                //Session is set during operations on other endpoints
-                //that can redirect to index
-                if let Some(flash) = session::get_flash(&session)? {
-                    context.insert("msg", &(flash.kind, escape_html(&flash.message)));
-                    session::clear_flash(&session);
-                }
+    let mut context = Context::new();
+    context.insert("tasks", &tasks);
 
-                let rendered =
-                    tmpl.render("index.html.tera", &context).map_err(|e| {
-                        error::ErrorInternalServerError(e.description().to_owned())
-                    })?;
+    // Session is set during operations on other endpoints that can redirect to index
+    if let Some(flash) = session::get_flash(&session)? {
+        context.insert("msg", &(flash.kind, flash.message));
+        session::clear_flash(&session);
+    }
 
-                Ok(HttpResponse::Ok().body(rendered))
-            }
-            Err(e) => Err(e),
-        })
+    let rendered = tmpl
+        .render("index.html.tera", &context)
+        .map_err(error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().body(rendered))
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct CreateForm {
     description: String,
 }
 
-pub fn create(
+pub async fn create(
     params: web::Form<CreateForm>,
-    pool: web::Data<db::PgPool>,
+    pool: web::Data<PgPool>,
     session: Session,
-) -> impl Future<Item = HttpResponse, Error = Error> {
+) -> Result<impl Responder, Error> {
     if params.description.is_empty() {
-        Either::A(
-            session::set_flash(
-                &session,
-                FlashMessage::error("Description cannot be empty"),
-            )
-            .map(|_| redirect_to("/"))
-            .into_future(),
-        )
+        session::set_flash(&session, FlashMessage::error("Description cannot be empty"))?;
+        Ok(web::Redirect::to("/").using_status_code(StatusCode::FOUND))
     } else {
-        Either::B(
-            web::block(move || db::create_task(params.into_inner().description, &pool))
-                .from_err()
-                .then(move |res| match res {
-                    Ok(_) => {
-                        session::set_flash(
-                            &session,
-                            FlashMessage::success("Task successfully added"),
-                        )?;
-                        Ok(redirect_to("/"))
-                    }
-                    Err(e) => Err(e),
-                }),
-        )
+        db::create_task(params.into_inner().description, &pool)
+            .await
+            .map_err(error::ErrorInternalServerError)?;
+
+        session::set_flash(&session, FlashMessage::success("Task successfully added"))?;
+
+        Ok(web::Redirect::to("/").using_status_code(StatusCode::FOUND))
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct UpdateParams {
     id: i32,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct UpdateForm {
     _method: String,
 }
 
-pub fn update(
-    db: web::Data<db::PgPool>,
+pub async fn update(
+    db: web::Data<PgPool>,
     params: web::Path<UpdateParams>,
     form: web::Form<UpdateForm>,
     session: Session,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    match form._method.as_ref() {
-        "put" => Either::A(Either::A(toggle(db, params))),
-        "delete" => Either::A(Either::B(delete(db, params, session))),
+) -> Result<impl Responder, Error> {
+    Ok(web::Redirect::to(match form._method.as_ref() {
+        "put" => toggle(db, params).await?,
+        "delete" => delete(db, params, session).await?,
         unsupported_method => {
-            let msg = format!("Unsupported HTTP method: {}", unsupported_method);
-            Either::B(err(error::ErrorBadRequest(msg)))
+            let msg = format!("Unsupported HTTP method: {unsupported_method}");
+            return Err(error::ErrorBadRequest(msg));
         }
-    }
+    })
+    .using_status_code(StatusCode::FOUND))
 }
 
-fn toggle(
-    pool: web::Data<db::PgPool>,
+async fn toggle(
+    pool: web::Data<PgPool>,
     params: web::Path<UpdateParams>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || db::toggle_task(params.id, &pool))
-        .from_err()
-        .then(move |res| match res {
-            Ok(_) => Ok(redirect_to("/")),
-            Err(e) => Err(e),
-        })
+) -> Result<&'static str, Error> {
+    db::toggle_task(params.id, &pool)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+    Ok("/")
 }
 
-fn delete(
-    pool: web::Data<db::PgPool>,
+async fn delete(
+    pool: web::Data<PgPool>,
     params: web::Path<UpdateParams>,
     session: Session,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || db::delete_task(params.id, &pool))
-        .from_err()
-        .then(move |res| match res {
-            Ok(_) => {
-                session::set_flash(
-                    &session,
-                    FlashMessage::success("Task was deleted."),
-                )?;
-                Ok(redirect_to("/"))
-            }
-            Err(e) => Err(e),
-        })
-}
+) -> Result<&'static str, Error> {
+    db::delete_task(params.id, &pool)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
 
-fn redirect_to(location: &str) -> HttpResponse {
-    HttpResponse::Found()
-        .header(http::header::LOCATION, location)
-        .finish()
+    session::set_flash(&session, FlashMessage::success("Task was deleted."))?;
+
+    Ok("/")
 }
 
 pub fn bad_request<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     let new_resp = NamedFile::open("static/errors/400.html")?
-        .set_status_code(res.status())
-        .respond_to(res.request())?;
-    Ok(ErrorHandlerResponse::Response(
-        res.into_response(new_resp.into_body()),
-    ))
+        .customize()
+        .with_status(res.status())
+        .respond_to(res.request())
+        .map_into_boxed_body()
+        .map_into_right_body();
+
+    Ok(ErrorHandlerResponse::Response(res.into_response(new_resp)))
 }
 
 pub fn not_found<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     let new_resp = NamedFile::open("static/errors/404.html")?
-        .set_status_code(res.status())
-        .respond_to(res.request())?;
-    Ok(ErrorHandlerResponse::Response(
-        res.into_response(new_resp.into_body()),
-    ))
+        .customize()
+        .with_status(res.status())
+        .respond_to(res.request())
+        .map_into_boxed_body()
+        .map_into_right_body();
+
+    Ok(ErrorHandlerResponse::Response(res.into_response(new_resp)))
 }
 
-pub fn internal_server_error<B>(
-    res: dev::ServiceResponse<B>,
-) -> Result<ErrorHandlerResponse<B>> {
+pub fn internal_server_error<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     let new_resp = NamedFile::open("static/errors/500.html")?
-        .set_status_code(res.status())
-        .respond_to(res.request())?;
-    Ok(ErrorHandlerResponse::Response(
-        res.into_response(new_resp.into_body()),
-    ))
+        .customize()
+        .with_status(res.status())
+        .respond_to(res.request())
+        .map_into_boxed_body()
+        .map_into_right_body();
+
+    Ok(ErrorHandlerResponse::Response(res.into_response(new_resp)))
 }
